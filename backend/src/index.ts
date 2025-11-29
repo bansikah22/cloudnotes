ca53e535516976d2e992920cd9c5683d81d7612a
t-desmond <desmondtardzenyuy@gmail.com>
2025-11-29 15:23:24 +0100

    feat(backend): Integrate CORS middleware and update package dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import notesRouter from "./routes.js"
+import cors from "cors";
 
 const PORT = process.env['PORT'] || 5000;
 const HOST = process.env['HOST'] || '0.0.0.0';
 const app = express();
+const corsOptions = {
+  origin: `${process.env['FRONTEND_URL'] || 'http://localhost:3000'}`,
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'application/json']
+};
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (_req : express.Request, res : express.Response) => {
     console.log("[GET]: /")
